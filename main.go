b034f4fc5e696fe04667f868a7510431b6ec5290
Mico Wijaya <micowijaya57@gmail.com>
2023-11-04 11:49:23 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/golang/golang-login/initial"
+
+	"github.com/golang/golang-login/model"
 
 	"github.com/golang/golang-login/migrate"
 
@@ -14,7 +18,20 @@ func init() {
 	migrate.Migrate()
 }
 
+func registerUser(c *gin.Context) {
+	var newUser model.Userdata
+
+	if err := c.BindJSON(&newUser); err != nil {
+		return
+	}
+
+	initial.DB.Create(newUser)
+
+	c.IndentedJSON(http.StatusCreated, newUser)
+}
+
 func main() {
 	router := gin.Default()
+	router.POST("/register", registerUser)
 	router.Run()
 }
