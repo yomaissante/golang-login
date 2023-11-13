37487d547c52d734986120ce168cf8d4b20ed5b8
Mico Wijaya <micowijaya57@gmail.com>
2023-11-13 15:50:53 +0700

    Update tokenGenerate.go

--- auth/tokenGenerate.go
+++ auth/tokenGenerate.go
@@ -16,10 +16,26 @@ import (
 	jwt "github.com/golang-jwt/jwt/v5"
 )
 
-func generateJWT() (string, error) {
-	token := jwt.New(jwt.SigningMethodEdDSA)
+var secret =  []byte(os.Getenv("SECRET_KEY"))
 
+func generateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["exp"] = time.Now().Add(30 * time.Minute)
+
+	claims["authorized"] = true
+
+	claims["user"] = "username"
+
+	tokenString, err := token.SignedString(secret)
+
+	if err != nil {
+		return "", err
+	 }
 	
+	 return tokenString, nil
 }
 
 
